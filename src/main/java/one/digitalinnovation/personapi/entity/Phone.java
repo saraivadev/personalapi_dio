2988979a50df988864f8c2d9f7c702ebc6977cf7
saraivadev <wsaraiva@outlook.com>
2021-08-12 22:35:59 -0300

    Confirguração do banco de dados no arquivo application.properties e persistencia das classes.

---
+++ src/main/java/one/digitalinnovation/personapi/entity/Phone.java
@@ -0,0 +1,25 @@
+package one.digitalinnovation.personapi.entity;
+
+import lombok.*;
+import one.digitalinnovation.personapi.enums.PhoneType;
+
+import javax.persistence.*;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+public class Phone {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PhoneType type;
+
+    @Column(nullable = false)
+    private  String number;
+}
