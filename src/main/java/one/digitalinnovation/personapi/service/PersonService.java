d5060e8d9944a065effd32fe1e2fa33d15a8a96a
saraivadev <wsaraiva@outlook.com>
2021-08-21 11:57:40 -0300

    Developped list all method

--- src/main/java/one/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -1,27 +1,46 @@
 package one.digitalinnovation.personapi.service;
 
-import one.digitalinnovation.personapi.dto.MessageResponseDTO;
+import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
+import one.digitalinnovation.personapi.dto.request.PersonDTO;
 import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.mapper.PersonMapper;
 import one.digitalinnovation.personapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PersonService {
     private PersonRepository personRepository;
-@Autowired
 
+    private final PersonMapper personMapper = PersonMapper.INSTANCE;
+
+
+    @Autowired
     public PersonService(PersonRepository personRepository) {
+
         this.personRepository = personRepository;
     }
 
-    public MessageResponseDTO createPerson(Person person){
+    public MessageResponseDTO createPerson(PersonDTO personDTO) {
 
-        Person savedPerson = personRepository.save(person);
-        return  MessageResponseDTO
+        Person personToSave = personMapper.toModel(personDTO);
+
+        Person savedPerson = personRepository.save(personToSave);
+
+        return MessageResponseDTO
                 .builder()
-                .message("Create person with ID"+savedPerson.getId())
+                .message("Create person with ID" + savedPerson.getId())
                 .build();
+    }
+
+    public List<PersonDTO> listAll() {
+        List<Person> allPeople = personRepository.findAll();
+
+        return allPeople.stream()
+                .map(personMapper::toDTO)
+                .collect(Collectors.toList());
     }
 }
